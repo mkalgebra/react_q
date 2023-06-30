783bc624a9f2c6fa7a52083c0a2e6fdcbace5550
Marijo Kis <marijo.kis@q.agency>
2023-06-30 15:11:30 +0200

    chore: test commit

--- src/core/hoc/layout/Layout.tsx
+++ src/core/hoc/layout/Layout.tsx
@@ -1,7 +1,7 @@
 import "./Layout.scss";
+import { ReactNode } from "react";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import { ReactNode } from "react";
 
 interface ChildrenInterface {
   children: ReactNode;
